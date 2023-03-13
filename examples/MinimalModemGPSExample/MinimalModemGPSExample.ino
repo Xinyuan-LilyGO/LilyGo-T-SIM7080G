9a430b096f86ee7afc9c227c3e8369c9fbab7dbf
lewishe <lewishe@outlook.com>
2023-03-13 19:35:11 +0800

    Added GNSS Config Example

--- examples/MinimalModemGPSExample/MinimalModemGPSExample.ino
+++ examples/MinimalModemGPSExample/MinimalModemGPSExample.ino
@@ -111,10 +111,59 @@ void setup()
     Serial.println();
     Serial.print("Modem started!");
 
-
     /*********************************
      * step 3 : start modem gps function
     ***********************************/
+
+    //  When configuring GNSS, you need to stop GPS first
+    modem.disableGPS();
+    delay(500);
+
+
+    /*
+    ! GNSS Work Mode Set
+     <gps mode> GPS work mode.
+        1 Start GPS NMEA out.
+    <glo mode> GLONASS work mode.
+        0 Stop GLONASS NMEA out.
+        1 Start GLONASS NMEA out.
+    <bd mode> BEIDOU work mode.
+        0 Stop BEIDOU NMEA out.
+        1 Start BEIDOU NMEA out.
+    <gal mode> GALILEAN work mode.
+        0 Stop GALILEAN NMEA out.
+        1 Start GALILEAN NMEA out.
+    <qzss mode> QZSS work mode.
+        0 Stop QZSS NMEA out.
+        1 Start QZSS NMEA out.*/
+
+    //GNSS Work Mode Set GPS+BEIDOU
+    modem.sendAT("+CGNSMOD=1,1,0,0,0");
+    modem.waitResponse();
+
+
+    /*
+    GNSS Command,For more parameters, see <SIM7070_SIM7080_SIM7090 Series_AT Command Manual> 212 page.
+    <minInterval> range: 1000-60000 ms
+     minInterval is the minimum time interval in milliseconds that must elapse between position reports. default value is 1000.
+    <minDistance> range: 0-1000
+     Minimum distance in meters that must be traversed between position reports. Setting this interval to 0 will be a pure time-based tracking/batching.
+    <accuracy>:
+        0  Accuracy is not specified, use default.
+        1  Low Accuracy for location is acceptable.
+        2 Medium Accuracy for location is acceptable.
+        3 Only High Accuracy for location is acceptable.
+    */
+    // minInterval = 1000,minDistance = 0,accuracy = 0
+    modem.sendAT("+SGNSCMD=2,1000,0,0");
+    modem.waitResponse();
+
+    // Turn off GNSS.
+    modem.sendAT("+SGNSCMD=0");
+    modem.waitResponse();
+
+    delay(500);
+
     // GPS function needs to be enabled for the first use
     if (modem.enableGPS() == false) {
         Serial.print("Modem enable gps function failed!!");
