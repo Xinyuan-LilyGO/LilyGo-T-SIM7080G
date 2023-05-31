5acbaac726b09bd69104b21a71d53ed0f3dab89c
Jun Wen <130400462+bootcampiot@users.noreply.github.com>
2023-05-31 11:31:25 -0700

    Add files via upload

---
+++ examples/SIM7080G-ATT-NB-IOT-AWS-MQTT/certs/AWS_root_CA.h
@@ -0,0 +1,8 @@
+#include <pgmspace.h>
+
+// Amazon Root CA 1 AWS VeriSign-Class 3-Public-Primary-Certification-Authority-G5
+static const char root_CA[] PROGMEM = R"EOF(
+-----BEGIN ROOT CERTIFICATE-----
+// Please copy here https://www.websecurity.digicert.com/content/dam/websitesecurity/digitalassets/desktop/pdfs/roots/VeriSign-Class%203-Public-Primary-Certification-Authority-G5.pem
+-----END ROOT CERTIFICATE-----
+)EOF";
