767710ed6a9da2ab14c8b0e1daf5600481a99415
Jun Wen <130400462+bootcampiot@users.noreply.github.com>
2023-05-31 11:38:17 -0700

    Add files via upload

---
+++ examples/SIM7080G-ATT-NB-IOT-SSL-Mosquitto/certs/Mosquitto_Client_PSK.h
@@ -0,0 +1,8 @@
+#include <pgmspace.h>
+
+// Device Private Key                                               
+static const char Client_PSK[] PROGMEM = R"EOF(
+-----BEGIN RSA PRIVATE KEY-----
+// Please generate the device private key as the guidance in https://test.mosquitto.org/ssl and paste it here//
+-----END RSA PRIVATE KEY-----
+)EOF";
