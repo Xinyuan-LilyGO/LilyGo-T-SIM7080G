64304d858ad9ad6e3f0d7bf3476365a60b9c1a41
lewisxhe <lewishe@outlook.com>
2024-03-29 11:35:06 +0800

    Added charge debug example https://github.com/Xinyuan-LilyGO/LilyGo-T-SIM7080G/issues/49

---
+++ examples/MinimalPowersCurrentExample/MinimalPowersCurrentExample.ino
@@ -0,0 +1,210 @@
+/**
+ * @file      MinimalPowersCurrentExample.ino
+ * @author    Lewis He (lewishe@outlook.com)
+ * @license   MIT
+ * @copyright Copyright (c) 2024  Shenzhen Xin Yuan Electronic Technology Co., Ltd
+ * @date      2024-03-29
+ * @note      For the inability to set the charging current, please check this post https://github.com/Xinyuan-LilyGO/LilyGo-T-SIM7080G/issues/49
+ * For setting a current greater than 500mA, the VBUS power supply must be sufficient. If the input is lower than 5V, the charging current will be below 500mA.
+ */
+#include <Arduino.h>
+#define XPOWERS_CHIP_AXP2101
+#include "XPowersLib.h"
+#include "utilities.h"
+
+XPowersPMU  PMU;
+
+bool  pmu_flag = false;
+uint32_t loopMillis;
+
+void setFlag(void)
+{
+    pmu_flag = true;
+}
+
+void printChargerConstantCurr()
+{
+    const uint16_t currTable[] = {
+        0, 0, 0, 0, 100, 125, 150, 175, 200, 300, 400, 500, 600, 700, 800, 900, 1000
+    };
+    uint8_t val = PMU.getChargerConstantCurr();
+    Serial.printf("Setting Charge Target Current - VAL:%u CURRENT:%u\n", val, currTable[val]);
+}
+
+void setup()
+{
+
+    Serial.begin(115200);
+
+    //Start while waiting for Serial monitoring
+    while (!Serial);
+
+    delay(3000);
+
+    Serial.println();
+
+    /*********************************
+     *  step 1 : Initialize power chip,
+     *  turn on modem and gps antenna power channel
+    ***********************************/
+    bool res;
+    //Use Wire1
+    res = PMU.begin(Wire1, AXP2101_SLAVE_ADDRESS, I2C_SDA, I2C_SCL);
+    if (!res) {
+        Serial.println("Failed to initialize power.....");
+        while (1) {
+            delay(5000);
+        }
+    }
+
+
+    // I2C sensor call example
+    int sda = 13;  // You can also use other IO ports
+    int scl = 21;  // You can also use other IO ports
+    Wire.begin(sda, scl);
+
+    //**\
+
+    //Other i2c sensors can be externally connected to 13,21
+
+    //**\
+
+
+    Serial.printf("getID:0x%x\n", PMU.getChipID());
+
+    // Set the minimum common working voltage of the PMU VBUS input,
+    // below this value will turn off the PMU
+    PMU.setVbusVoltageLimit(XPOWERS_AXP2101_VBUS_VOL_LIM_4V36);
+
+    // Set the maximum current of the PMU VBUS input,
+    // higher than this value will turn off the PMU
+    PMU.setVbusCurrentLimit(XPOWERS_AXP2101_VBUS_CUR_LIM_1500MA);
+
+    // Set VSY off voltage as 2600mV , Adjustment range 2600mV ~ 3300mV
+    PMU.setSysPowerDownVoltage(2600);
+
+    // TS Pin detection must be disable, otherwise it cannot be charged
+    PMU.disableTSPinMeasure();
+
+    /*********************************
+     * step 2 : Enable internal ADC detection
+    ***********************************/
+    PMU.enableBattDetection();
+    PMU.enableVbusVoltageMeasure();
+    PMU.enableBattVoltageMeasure();
+    PMU.enableSystemVoltageMeasure();
+
+
+    /*********************************
+     * step 3 : Set PMU interrupt
+    ***********************************/
+    pinMode(PMU_INPUT_PIN, INPUT);
+    attachInterrupt(PMU_INPUT_PIN, setFlag, FALLING);
+
+    // Disable all interrupts
+    PMU.disableIRQ(XPOWERS_AXP2101_ALL_IRQ);
+    // Clear all interrupt flags
+    PMU.clearIrqStatus();
+    // Enable the required interrupt function
+    PMU.enableIRQ(
+        XPOWERS_AXP2101_BAT_INSERT_IRQ    | XPOWERS_AXP2101_BAT_REMOVE_IRQ      |   //BATTERY
+        XPOWERS_AXP2101_VBUS_INSERT_IRQ   | XPOWERS_AXP2101_VBUS_REMOVE_IRQ     |   //VBUS
+        XPOWERS_AXP2101_PKEY_SHORT_IRQ    | XPOWERS_AXP2101_PKEY_LONG_IRQ       |   //POWER KEY
+        XPOWERS_AXP2101_BAT_CHG_DONE_IRQ  | XPOWERS_AXP2101_BAT_CHG_START_IRQ       //CHARGE
+    );
+
+    /*********************************
+     * step 4 : Set PMU Charger params
+    ***********************************/
+    // Set the precharge charging current
+    PMU.setPrechargeCurr(XPOWERS_AXP2101_PRECHARGE_50MA);
+    // Set constant current charge current limit , 0mA disable charge
+    PMU.setChargerConstantCurr(XPOWERS_AXP2101_CHG_CUR_0MA);
+    // Set charge cut-off voltage
+    PMU.setChargeTargetVoltage(XPOWERS_AXP2101_CHG_VOL_4V2);
+
+
+    /*********************************
+     * step 5 : Disable all power channel , Used to monitor charging current and exclude other load current consumption
+    ***********************************/
+    PMU.disableDC2();
+    PMU.disableDC3();
+    PMU.disableDC4();
+    PMU.disableDC5();
+
+    PMU.disableALDO1();
+    PMU.disableALDO2();
+    PMU.disableALDO3();
+    PMU.disableALDO4();
+    PMU.disableBLDO1();
+    PMU.disableBLDO2();
+
+    PMU.disableCPUSLDO();
+    PMU.disableDLDO1();
+    PMU.disableDLDO2();
+
+    Serial.println("DCDC=======================================================================");
+    Serial.printf("DC1  : %s   Voltage:%u mV \n",  PMU.isEnableDC1()  ? "+" : "-", PMU.getDC1Voltage());
+    Serial.printf("DC2  : %s   Voltage:%u mV \n",  PMU.isEnableDC2()  ? "+" : "-", PMU.getDC2Voltage());
+    Serial.printf("DC3  : %s   Voltage:%u mV \n",  PMU.isEnableDC3()  ? "+" : "-", PMU.getDC3Voltage());
+    Serial.printf("DC4  : %s   Voltage:%u mV \n",  PMU.isEnableDC4()  ? "+" : "-", PMU.getDC4Voltage());
+    Serial.printf("DC5  : %s   Voltage:%u mV \n",  PMU.isEnableDC5()  ? "+" : "-", PMU.getDC5Voltage());
+    Serial.println("ALDO=======================================================================");
+    Serial.printf("ALDO1: %s   Voltage:%u mV\n",  PMU.isEnableALDO1()  ? "+" : "-", PMU.getALDO1Voltage());
+    Serial.printf("ALDO2: %s   Voltage:%u mV\n",  PMU.isEnableALDO2()  ? "+" : "-", PMU.getALDO2Voltage());
+    Serial.printf("ALDO3: %s   Voltage:%u mV\n",  PMU.isEnableALDO3()  ? "+" : "-", PMU.getALDO3Voltage());
+    Serial.printf("ALDO4: %s   Voltage:%u mV\n",  PMU.isEnableALDO4()  ? "+" : "-", PMU.getALDO4Voltage());
+    Serial.println("BLDO=======================================================================");
+    Serial.printf("BLDO1: %s   Voltage:%u mV\n",  PMU.isEnableBLDO1()  ? "+" : "-", PMU.getBLDO1Voltage());
+    Serial.printf("BLDO2: %s   Voltage:%u mV\n",  PMU.isEnableBLDO2()  ? "+" : "-", PMU.getBLDO2Voltage());
+    Serial.println("CPUSLDO====================================================================");
+    Serial.printf("CPUSLDO: %s Voltage:%u mV\n",  PMU.isEnableCPUSLDO() ? "+" : "-", PMU.getCPUSLDOVoltage());
+    Serial.println("DLDO=======================================================================");
+    Serial.printf("DLDO1: %s   Voltage:%u mV\n",  PMU.isEnableDLDO1()  ? "+" : "-", PMU.getDLDO1Voltage());
+    Serial.printf("DLDO2: %s   Voltage:%u mV\n",  PMU.isEnableDLDO2()  ? "+" : "-", PMU.getDLDO2Voltage());
+    Serial.println("===========================================================================");
+
+    /*
+    The default setting is CHGLED is automatically controlled by the PMU.
+    - XPOWERS_CHG_LED_OFF,
+    - XPOWERS_CHG_LED_BLINK_1HZ,
+    - XPOWERS_CHG_LED_BLINK_4HZ,
+    - XPOWERS_CHG_LED_ON,
+    - XPOWERS_CHG_LED_CTRL_CHG,
+    * */
+    PMU.setChargingLedMode(XPOWERS_CHG_LED_ON);
+
+    // Print default setting current
+    printChargerConstantCurr();
+}
+
+uint8_t curIndex = 4;
+
+void loop()
+{
+    if (pmu_flag) {
+        pmu_flag = false;
+        // Get PMU Interrupt Status Register
+        uint32_t status = PMU.getIrqStatus();
+        if (PMU.isPekeyShortPressIrq()) {
+
+            // Set constant current charge current limit
+            // For setting a current greater than 500mA, the VBUS power supply must be sufficient. If the input is lower than 5V, the charging current will be below 500mA.
+            if (!PMU.setChargerConstantCurr(curIndex)) {
+                Serial.println("Setting Charger Constant Current Failed!");
+            }
+            printChargerConstantCurr();
+            Serial.println("===========================");
+            curIndex++;
+            curIndex %= (XPOWERS_AXP2101_CHG_CUR_1000MA + 1);
+
+            // Toggle CHG led
+            PMU.setChargingLedMode(PMU.getChargingLedMode() != XPOWERS_CHG_LED_OFF ? XPOWERS_CHG_LED_OFF : XPOWERS_CHG_LED_ON);
+        }
+        // Clear PMU Interrupt Status Register
+        PMU.clearIrqStatus();
+    }
+
+    delay(200);
+}
+
